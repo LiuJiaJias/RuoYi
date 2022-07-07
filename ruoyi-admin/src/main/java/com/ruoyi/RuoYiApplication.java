de99d63c442140683e7a365993e578afe801bf7d
刘佳佳 <850354060@qq.com>
2022-07-07 09:37:40 +0800

    修改端口号-

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -18,7 +18,7 @@ public class RuoYiApplication
     {
          System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
-        String message = "w我的若依学习旅程开启";
+        String message = "w我的若依学习旅程开启！";
         System.out.println("args = " + message);
         System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
                 " .-------.       ____     __        \n" +
